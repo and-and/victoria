7fbc1c581789729653ba10d6e9dfbfecf4963895
and_and <and_and@me.com>
2013-08-28 23:36:21 +0300

    Search header

--- sites/all/themes/victoria/scripts/vs-main.js
+++ sites/all/themes/victoria/scripts/vs-main.js
@@ -1,6 +1,7 @@
 (function($) {
   Drupal.behaviors.searchHeaderRedirect = {
     attach:function() {
+      jQuery('#searchform input').watermark('Поиск', {useNative: false});
       $('#searchform .vs-header-search-submit').click(function(){
         var path = '/product/search/' + $(this).parent().find('input[type=text]').val();
         document.location.href = path;
