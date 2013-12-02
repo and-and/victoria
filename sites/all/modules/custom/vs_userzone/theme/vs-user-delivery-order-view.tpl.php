e797dd71d1e700a788d34ed1aaf1f7de4d3b1577
and_and <and_and@me.com>
2013-12-02 22:53:04 +0200

    Update core

--- sites/all/modules/custom/vs_userzone/theme/vs-user-delivery-order-view.tpl.php
+++ sites/all/modules/custom/vs_userzone/theme/vs-user-delivery-order-view.tpl.php
@@ -9,21 +9,6 @@
         <span><?php print t('Номер склада новой почты: ') . $billing['n_sclada'];?></span>
       </div>
     <h2><?php print t('Продукты:'); ?></h2>
-      <table class="vs-orders-list">
-        <tr>
-          <th><?php print t('Код товара'); ?></th>
-          <th><?php print t('Размер'); ?></th>
-          <th><?php print t('Цвет'); ?></th>
-          <th><?php print t('Цена'); ?></th>
-        </tr>
-        <?php foreach ($products as $product) : ?>
-          <tr>
-            <?php foreach ($product as $val) : ?>
-              <td><?php print $val; ?></td>
-            <?php endforeach; ?>
-          </tr>
-        <?php endforeach; ?>
-      </table>
   </div>
   <div class="clearfix"></div>
 </div>
